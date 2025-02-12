bfd6a651e1767f79c7cc14caf0e7b03fc1eb739b
Ankur Hirwani <73126292+ankur-hirwani@users.noreply.github.com>
2025-02-12 13:22:15 +0530

    Create add.cpp

---
+++ BufferOverFLow/vulnerable/add.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+const char *PASSWORD_FILE = "rictro";
+int add(int x,int y)
+{
+    return x+y;
+}
+int main()
+{
+    
+int x,y;
+cin>>x>>y;
+cout<<add(x,y);
+}
